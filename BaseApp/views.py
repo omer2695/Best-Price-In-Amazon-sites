0835fc37d001a9db68114b35fefe3c929476f8c0
omer2695 <82286519+omer2695@users.noreply.github.com>
2023-02-09 21:34:42 +0200

    Main template + navigation bar
    i added new file Main.html,
    this file will contain the main templates for the web pages

--- BaseApp/views.py
+++ BaseApp/views.py
@@ -12,14 +12,3 @@ def Compare_Page(request):
     return render(request, 'Compare_Page.html')
 
 
-    """need to change it to the tample of the search page here
-    for example:
-        def best_to_compare(request):
-            amazonSites = Member.objects.all().values()
-            template = loader.get_template('amazon_sites.html')
-            context = {
-                'amazonSites': amazonSites,
-            }
-            return HttpResponse(template.render(context, request))
-    """
-
