c210d662d6b22d4c8d4820d69f85039b51bc2c2d
omer2695 <82286519+omer2695@users.noreply.github.com>
2023-02-22 19:25:31 +0200

    fix search button
    not when user will press on search with no input it will return no result

--- BaseApp/views.py
+++ BaseApp/views.py
@@ -15,7 +15,7 @@ def SearchPage(request):
 
 
 def Compare_Page(request):
-    search_term = request.GET.get('search_term', 'laptop')  # Default to "laptop" if search term is not provided
+    search_term = request.GET.get('search_term')  # Default to "laptop" if search term is not provided
 
     # Create a list of Amazon URLs with the search term
     urls = [
@@ -26,41 +26,43 @@ def Compare_Page(request):
     ]
 
     results = []  # List to store search results
+    if search_term:
+        # Loop through the URLs and scrape the search results
+        for url in urls:
+            html = requests.get(url).text
+            soup = BeautifulSoup(html, 'html.parser')
 
-    # Loop through the URLs and scrape the search results
-    for url in urls:
-        html = requests.get(url).text
-        soup = BeautifulSoup(html, 'html.parser')
+            items = soup.find_all('div', {'data-component-type': 's-search-result'})
+            for item in items:
+                title_element = item.find('h2', {'class': 'a-size-mini'})
+                title = title_element.text.strip() if title_element else ''
 
-        items = soup.find_all('div', {'data-component-type': 's-search-result'})
-        for item in items:
-            title_element = item.find('h2', {'class': 'a-size-mini'})
-            title = title_element.text.strip() if title_element else ''
+                price_element = item.find('span', {'class': 'a-price-whole'})
+                price = price_element.text.replace(',', '.') if price_element else 'N/A'
 
-            price_element = item.find('span', {'class': 'a-price-whole'})
-            price = price_element.text.replace(',', '.') if price_element else 'N/A'
+                image_element = item.find('img', {'class': 's-image'})
+                image_url = image_element['src'] if image_element else ''
 
-            image_element = item.find('img', {'class': 's-image'})
-            image_url = image_element['src'] if image_element else ''
+                url_element = item.find('a', {'class': 'a-link-normal'})
+                product_url = url_element['href'] if url_element else ''
 
-            url_element = item.find('a', {'class': 'a-link-normal'})
-            product_url = url_element['href'] if url_element else ''
+                currency_symbol_element = item.find('span', {'class': 'a-price-symbol'})
+                currency_symbol = currency_symbol_element.text.strip() if currency_symbol_element else ''
 
-            currency_symbol_element = item.find('span', {'class': 'a-price-symbol'})
-            currency_symbol = currency_symbol_element.text.strip() if currency_symbol_element else ''
+                results.append({
+                    'title': title,
+                    'price': price,
+                    'image_url': image_url,
+                    'product_url': product_url,
+                    'currency_symbol': currency_symbol
+                })
+        # Create a dictionary to pass to the template
+        context = {
+            'search_term': search_term,
+            'results': results,
+        }
 
-            results.append({
-                'title': title,
-                'price': price,
-                'image_url': image_url,
-                'product_url': product_url,
-                'currency_symbol': currency_symbol
-            })
-    # Create a dictionary to pass to the template
-    context = {
-        'search_term': search_term,
-        'results': results,
-    }
-
-    # Render the template with the context
-    return render(request, 'Compare_Page.html', context)
+        # Render the template with the context
+        return render(request, 'Compare_Page.html', context)
+    else:
+        return render(request, 'Compare_Page.html')
