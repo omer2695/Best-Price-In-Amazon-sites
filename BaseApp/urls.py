288a6e860b461d5138e102de01ad5e7d0a158602
omer2695 <82286519+omer2695@users.noreply.github.com>
2023-02-22 15:43:58 +0200

    new home page
    - new home/landing page

--- BaseApp/urls.py
+++ BaseApp/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns =[
     path('', views.HomePage, name='HomePage'),
-    path('SearchPage/', views.SearchPage, name='SearchPage'),
     path('Compare_Page/', views.Compare_Page, name='Compare_Page'),
 ]
